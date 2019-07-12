09df3089441b95a5329bd11024d505f423751bf7
pynickle <47242104+code-nick-python@users.noreply.github.com>
2019-07-12 16:28:13 +0800

    add web

---
+++ www/src/browser/index.js
@@ -0,0 +1,31 @@
+var app = {
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+    onDeviceReady: function () {
+        this.receivedEvent('deviceready');
+    },
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
+
+document.getElementById("openBrowser").addEventListener("click", openBrowser);
+
+function openBrowser() {
+    var ref = window.open("https://www.baidu.com", "_blank")
+    ref.addEventListener("exit", browserExit)
+
+    function browserExit() {
+        ref.close();
+    }
+}
